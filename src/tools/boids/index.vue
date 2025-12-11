c65f38b059184f731ffb4cd4af9cafadd7472a0b
‘yuchane’ <2198469219@qq.com>
2025-12-11 15:21:33 +0800

    feat: 添加lil-gui进行参数调节

--- src/tools/boids/index.vue
+++ src/tools/boids/index.vue
@@ -1,47 +1,30 @@
 <template>
     <div>
         <canvas ref="canvasRef"></canvas>
-        <div class="attrs">
-            <div class="attrs-item">
-                <div class="label">
-                    分离权重
-                </div>
-                <div class="value">
-                    <input type="range" min="0" max="2" step="0.01" v-model="separationWeight"></input>
-                </div>
-            </div>
-            <div class="attrs-item">
-                <div class="label">
-                    对齐权重
-                </div>
-                <div class="value">
-                    <input type="range" min="0" max="2" step="0.01" v-model="alignmentWeight"></input>
-                </div>
-            </div>
-            <div class="attrs-item">
-                <div class="label">
-                    聚集权重
-                </div>
-                <div class="value">
-                    <input type="range" min="0" max="2" step="0.01" v-model="cohesionWeight"></input>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
 <script setup>
 import { onBeforeUnmount, onMounted, ref } from 'vue';
 import Boid from './boids';
+import GUI from 'lil-gui';
 
 const canvasRef = ref();
 let ctx = null;
 const dpr = window.devicePixelRatio;
 let animationId = null;
 // 参数
-const separationWeight = ref(1.0);
-const alignmentWeight = ref(1.0);
-const cohesionWeight = ref(1.0);
+const data = {
+    separationWeight: 1.0,
+    alignmentWeight: 1.0,
+    cohesionWeight: 1.0
+}
+
+// 参数调节
+const gui = new GUI({ title: '参数调节' });
+gui.add(data, 'separationWeight', 0, 2).step(0.01).name('分离权重');
+gui.add(data, 'alignmentWeight', 0, 2).step(0.01).name('对齐权重');
+gui.add(data, 'cohesionWeight', 0, 2).step(0.01).name('聚集权重');
 
 function resizeCanvas() {
     canvasRef.value.width = window.innerWidth * dpr;
@@ -61,9 +44,9 @@ function animate() {
     // 更新并绘制所有Boid
     for (const boid of boids) {
         boid.flock(boids, {
-            separationWeight: separationWeight.value,
-            alignmentWeight: alignmentWeight.value,
-            cohesionWeight: cohesionWeight.value
+            separationWeight: data.separationWeight,
+            alignmentWeight: data.alignmentWeight,
+            cohesionWeight: data.cohesionWeight
         });
         boid.update();
         boid.draw();
@@ -84,64 +67,9 @@ onMounted(() => {
 onBeforeUnmount(() => {
     window.removeEventListener('resize', resizeCanvas);
     cancelAnimationFrame(animationId);
+    gui.destroy();
 })
 </script>
 
 <style scoped>
-.attrs {
-    position: fixed;
-    top: 0;
-    right: 0;
-    display: flex;
-    flex-direction: column;
-    padding-top: 10px;
-}
-
-.attrs-item {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-    gap: 10px;
-}
-
-.attrs-item label {
-    font-size: 14px;
-    color: #333;
-}
-
-.attrs-item .value {
-    display: flex;
-    align-items: center;
-    width: 150px;
-}
-
-[type="range"] {
-    -webkit-appearance: none;
-    appearance: none;
-    margin: 0;
-    outline: 0;
-    background-color: transparent;
-    width: 120px;
-    height: 15px;
-}
-
-[type="range"]::-webkit-slider-runnable-track {
-    background: #0000004f;
-    height: 15px;
-}
-
-[type="range" i]::-webkit-slider-container {
-    height: 15px;
-    overflow: hidden;
-}
-
-[type="range"]::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    appearance: none;
-    width: 0px;
-    height: 15px;
-    border: 1px solid transparent;
-    border-image: linear-gradient(#2FA1D6, #2FA1D6) 0 fill / 0 0 0 0 / 0px 0px 0 200px;
-    cursor: e-resize;
-}
 </style>
